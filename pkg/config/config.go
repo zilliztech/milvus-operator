bfcbe1f52a9d60f36f55c0764f5133abac5d7ce9
shaoyue <shaoyue.chen@zilliz.com>
2024-01-02 11:14:40 +0800

    Update demo manifest, bump version (#60)
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultMilvusVersion   = "v2.3.3"
+	DefaultMilvusVersion   = "v2.3.4"
 	DefaultMilvusBaseImage = "milvusdb/milvus"
 	DefaultMilvusImage     = DefaultMilvusBaseImage + ":" + DefaultMilvusVersion
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
