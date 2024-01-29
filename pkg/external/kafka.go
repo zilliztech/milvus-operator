5af682742037ab14b87f956d26418738bd240d37
Sahil Devgon <sahildevgon.30@gmail.com>
2024-01-29 11:08:03 +0530

    Update kafka.go to fix typo and update config for external Kafka Connection in SASL mode (#72)
    * Update kafka.go
    
    Signed-off-by: SahilDevgon <37777565+SahilDevgon@users.noreply.github.com>
    
    * Update kafka.go | remove comment
    
    Signed-off-by: SahilDevgon <37777565+SahilDevgon@users.noreply.github.com>
    
    * pkg/kafka.go:46 Code is expecting SecurityProtocol in config, fixing same in document
    
    Signed-off-by: SahilDevgon <37777565+SahilDevgon@users.noreply.github.com>
    
    * Resolve issue with case-sensitivity
    
    Signed-off-by: Sahil Devgon <37777565+SahilDevgon@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: SahilDevgon <37777565+SahilDevgon@users.noreply.github.com>
    Signed-off-by: Sahil Devgon <37777565+SahilDevgon@users.noreply.github.com>

--- pkg/external/kafka.go
+++ pkg/external/kafka.go
@@ -108,7 +108,7 @@ func CheckKafka(conf CheckKafkaConfig) error {
 		ctx, cancel := context.WithTimeout(context.Background(), DependencyCheckTimeout)
 		defer cancel()
 		err := r.SetOffsetAt(ctx, time.Now())
-		return errors.Wrap(err, "check consume offset from borker failed")
+		return errors.Wrap(err, "check consume offset from broker failed")
 	}
 	return util.DoWithBackoff("checkKafka", checkKafka, util.DefaultMaxRetry, util.DefaultBackOffInterval)
 }
