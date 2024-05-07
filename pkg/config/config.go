101d563d1fa1425437c820385977d0dcc2511eb7
shaoyue.chen <shaoyue.chen@zilliz.com>
2024-05-07 10:12:17 +0800

    Milvus default version to 2.4.1, bump version
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultMilvusVersion   = "v2.4.0"
+	DefaultMilvusVersion   = "v2.4.1"
 	DefaultMilvusBaseImage = "milvusdb/milvus"
 	DefaultMilvusImage     = DefaultMilvusBaseImage + ":" + DefaultMilvusVersion
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
