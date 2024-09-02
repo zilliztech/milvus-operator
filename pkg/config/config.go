65f3ca795262f1f552a643a4553f79361b07270d
shaoyue <shaoyue.chen@zilliz.com>
2024-09-02 11:06:55 +0800

    milvus default to 2.4.10, bump version (#178)
    also update pymilvus to 2.4.6
    
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultMilvusVersion   = "v2.4.9"
+	DefaultMilvusVersion   = "v2.4.10"
 	DefaultMilvusBaseImage = "milvusdb/milvus"
 	DefaultMilvusImage     = DefaultMilvusBaseImage + ":" + DefaultMilvusVersion
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
