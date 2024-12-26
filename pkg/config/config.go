6b9d4da475fc74755a777d37006b80d5e927b2a7
haorenfsa <shaoyue.chen@zilliz.com>
2024-12-26 19:06:06 +0800

    milvus default to 2.4.19, supports imagePullSecrets for operator
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	DefaultMilvusVersion   = "v2.4.17"
+	DefaultMilvusVersion   = "v2.4.19"
 	DefaultMilvusBaseImage = "milvusdb/milvus"
 	DefaultMilvusImage     = DefaultMilvusBaseImage + ":" + DefaultMilvusVersion
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
